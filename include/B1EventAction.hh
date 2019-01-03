ac713c9931a77b283df3e22aa3f1689b92bc5c70
Elias Gerstmayr <elias.gerstmayr15@gmail.com>
2019-01-03 19:26:54 +0000

    Update B1EventAction.hh
    Added definitions in header for energy deposit in event actions.

--- include/B1EventAction.hh
+++ include/B1EventAction.hh
@@ -19,6 +19,8 @@ class B1EventAction : public G4UserEventAction
     void AddEdep(G4double edep) { fEdep += edep; }
     void AddEdep_Jena(G4double edep_Jena) { fEdep_Jena += edep_Jena; }
     void AddEdep_DESY(G4double edep_DESY) { fEdep_DESY += edep_DESY; }
+    void AddEdep_HDual(G4double edep_HDual) { fEdep_HDual += edep_HDual; }
+    void AddEdep_VDual(G4double edep_VDual) { fEdep_VDual += edep_VDual; }
 
     std::map<G4int, int> GetRRMap(){ return RR; };
     B1RunAction* GetRunAction(){ return fRunAction; };
@@ -28,6 +30,9 @@ class B1EventAction : public G4UserEventAction
     G4double     fEdep;
     G4double     fEdep_Jena;
     G4double     fEdep_DESY;
+    G4double     fEdep_HDual;
+    G4double     fEdep_VDual;
+
 
     // Here is a hashtable storing the track IDs which have been affected by radiation reaction
     std::map<G4int, int> RR;
